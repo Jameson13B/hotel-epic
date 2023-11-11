3de60628f55f23f1a4697ca03e5e655273be1111
Jameson Brown <jamesonbrown@neolivity.com>
2023-11-11 11:06:09 -0800

    Added loading image

--- src/components/AdminAccess.js
+++ src/components/AdminAccess.js
@@ -1,27 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import { ReactComponent as SearchLogo } from "../assets/search.svg";
+import loadingLogo from "../assets/loading.png";
 
 export const AdminAccess = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState("");
   const [event, setEvent] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
 
-  const getEvent = async (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    if (!submitting) return () => {};
 
     if (!searchTerm) {
       return;
     }
 
-    const results = await axios.get(
-      `.netlify/functions/getEvent?id=${searchTerm}`
-    );
+    axios
+      .get(`.netlify/functions/getEvent?id=${searchTerm}`)
+      .then((results) => {
+        setEvent({ ...results.data, id: searchTerm });
+        setSubmitting(false);
+      });
 
-    setEvent({ ...results.data, id: searchTerm });
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [submitting]);
 
   return (
     <React.Fragment>
@@ -39,19 +44,37 @@ export const AdminAccess = () => {
         <p className="text-[green] text-[1.15rem] font-bold mb-[16px]">
           *Authenticated*
         </p>
-        <form className="flex justify-center mb-[24px]" onSubmit={getEvent}>
+        <form
+          className="flex justify-center mb-[24px]"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setSubmitting(true);
+          }}
+        >
           <input
             className="bg-[#cacaca] text-[darkslategrey] font-semibold rounded-[6px] text-[2rem] text-center w-full"
             id="password"
             onChange={({ target }) => setSearchTerm(target.value)}
-            onSubmit={getEvent}
+            onSubmit={(e) => {
+              e.preventDefault();
+              setSubmitting(true);
+            }}
             value={searchTerm}
           />
           <button
             className="bg-[#282c34] border-[2px] border-white rounded-[8px] color-white text-[1rem] p-[8px] ml-[20px] font-bold w-[20%] flex justify-center"
             type="submit"
           >
-            <SearchLogo />
+            {submitting ? (
+              <img
+                width="50"
+                height="50"
+                src={loadingLogo}
+                alt="spinner-frame-5"
+              />
+            ) : (
+              <SearchLogo />
+            )}
           </button>
         </form>
         {event && (
@@ -65,7 +88,7 @@ export const AdminAccess = () => {
             <p className="text-[1.5rem] mt-[12px] italic font-bold">
               Attendees:
             </p>
-            {event.attendees.map((person) => (
+            {event.attendees?.map((person) => (
               <p key={person.contact}>
                 {person.name} - {person.contact}
               </p>
